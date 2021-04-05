28b840449bcf3bdef672c319a8a8dbad3b85c358
willycgm <80619452+willycgm@users.noreply.github.com>
2021-04-05 22:27:51 +0800

    Add files via upload

--- py0321/hw3.py
+++ py0321/hw3.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 21 16:00:40 2021
-
 @author: user
 """
 
@@ -23,40 +22,33 @@ for i in range(rows):
     for j in range(columns):
         # 將亂數產生的值放入串列中
         List[i].append(random.randint(1, 100))
-
 # 使用 for 迴圈來印出串列
 for i in range(len(List)):
     for j in range(len(List[i])):
         print('%5d' %(List[i][j]), end = '')
     print()
 '''
-data = []
-for i in range(5):
+indata = int(input("階數:"))
+data = list()
+for i in range(indata):  #0 - 5
     data.append([]) # 令其轉為二維串列形式
     #[[],[],[],[],[]]
-    for j in range(5):
-        data[i].append(0)#[0, 0, 0, 0, 0]
+    for j in range(0,i+1):
+        data[i].append(0)	
         
 data[0][0] = 1
-for i in range(1,5):
+for i in range(1,indata):
     data[i][0] = 1
     data[i][i] = 1
-print (data) 
-print()
-for i in range(2,5):
+ 	
+for i in range(2,indata):
     for j in range(1,i):
         #上一層的(前&後相加)        
         data[i][j] = data[i-1][j-1] + data[i-1][j]
-print (data)        
+		
+print (data)   
 
-
-
-
-
-
-
-
-
-
-
-
+for i in range(indata):
+    for j in range(0,i+1):
+        print (data[i][j] ,end=" ")
+    print () 
